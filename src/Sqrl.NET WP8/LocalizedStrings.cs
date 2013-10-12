80d02da67c0961f01c9b22ff57d43faf21242fb6
Leslie Hanks <leslie@lesliehanks.com>
2013-10-12 05:29:05 -0500

    Adding WP8 QR Reader

---
+++ src/Sqrl.NET WP8/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using Sqrl.NET_WP8.Resources;
+
+namespace Sqrl.NET_WP8 {
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings {
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
---
+++ src/Sqrl.NET WP8/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.Devices;
+using Microsoft.Phone.Controls;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace Sqrl.NET_WP8 {
+	public class PhotoLuminanceSource : LuminanceSource {
+		public byte[] PreviewBufferY { get; set; }
+
+		public override byte[] Matrix {
+			get { return (byte[]) (Array) PreviewBufferY; }
+		}
+
+		public PhotoLuminanceSource(int width, int height)
+			: base(width, height) {
+			PreviewBufferY = new byte[width*height];
+		}
+
+		public override byte[] getRow(int y, byte[] row) {
+			if (row == null || row.Length < Width) {
+				row = new byte[Width];
+			}
+
+			for (var i = 0; i < Height; i++) {
+				row[i] = (byte) PreviewBufferY[i*Width + y];
+			}
+
+			return row;
+		}
+	}
+
+	public partial class MainPage : PhoneApplicationPage {
+		private readonly ObservableCollection<string> _matches;
+		private readonly DispatcherTimer _timer;
+
+		private PhotoLuminanceSource _luminance;
+		private PhotoCamera _photoCamera;
+		private QRCodeReader _reader;
+
+		public MainPage() {
+			InitializeComponent();
+
+			_matches = new ObservableCollection<string>();
+			foundList.ItemsSource = _matches;
+
+			_timer = new DispatcherTimer();
+			_timer.Interval = TimeSpan.FromMilliseconds(250);
+			_timer.Tick += (o, arg) => ScanPreviewBuffer();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e) {
+			var app = Application.Current as App;
+//			app.ScannedValue = string.Empty;
+
+			_photoCamera = new PhotoCamera();
+			_photoCamera.Initialized += OnPhotoCameraInitialized;
+			_previewVideo.SetSource(_photoCamera);
+
+			CameraButtons.ShutterKeyHalfPressed += (o, arg) => _photoCamera.Focus();
+
+			base.OnNavigatedTo(e);
+		}
+
+		private void DisplayResult(string text) {
+			if (!_matches.Contains(text)) {
+				_matches.Add(text);
+			}
+		}
+
+		private void OnPhotoCameraInitialized(object sender, CameraOperationCompletedEventArgs e) {
+			var width = Convert.ToInt32(_photoCamera.PreviewResolution.Width);
+			var height = Convert.ToInt32(_photoCamera.PreviewResolution.Height);
+
+			_luminance = new PhotoLuminanceSource(width, height);
+			_reader = new QRCodeReader();
+
+			Dispatcher.BeginInvoke(() => {
+				_previewTransform.Rotation = _photoCamera.Orientation;
+				_timer.Start();
+			});
+		}
+
+		private void ScanPreviewBuffer() {
+			try {
+				_photoCamera.GetPreviewBufferY(_luminance.PreviewBufferY);
+				var binarizer = new HybridBinarizer(_luminance);
+				var binBitmap = new BinaryBitmap(binarizer);
+				var result = _reader.decode(binBitmap);
+				if (result != null) {
+					Dispatcher.BeginInvoke(() => DisplayResult(result.Text));
+				}
+			} catch {
+			}
+		}
+
+		private void foundList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		}
+	}
+}
